d47bec62e7fc76a8236a0813b76ce70728d9e459
Aristarh <aristiktop8@gmail.com>
2026-05-04 12:22:14 +0500

    fix: api prifix

--- src/libs/apollo-client.ts
+++ src/libs/apollo-client.ts
@@ -1,8 +1,16 @@
 import { InMemoryCache } from "@apollo/client";
 import { ApolloClient, createHttpLink } from "@apollo/client";
 
+const serverUrl =
+  process.env.NEXT_PUBLIC_SERVER_URL || "https://champino.org/api/graphql";
+
+// Log for debugging
+if (typeof window !== "undefined") {
+  console.log("[Apollo Client] Server URL:", serverUrl);
+}
+
 const httpLink = createHttpLink({
-  uri: process.env.NEXT_PUBLIC_SERVER_URL,
+  uri: serverUrl,
   credentials: "include",
 });
 
